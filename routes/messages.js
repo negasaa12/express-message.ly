9690dcf1946fc6683eeb3b2536b90f3d651896d5
kelly <sntsklvn@gmail.com>
2023-06-21 13:04:05 -0400

    making messages route work

--- routes/messages.js
+++ routes/messages.js
@@ -1,13 +1,11 @@
 const Router = require("express").Router;
-const User = require("../models/user");
-const {ensureLoggedIn, ensureCorrectUser} = require("../middleware/auth");
-
 const router = new Router();
 
+const Message = require("../models/message");
+const {ensureLoggedIn} = require("../middleware/auth");
+const ExpressError = require("../expressError");
 
-
-
-/** GET /:id - get detail of message.
+/** get detail of message.
  *
  * => {message: {id,
  *               body,
@@ -20,21 +18,75 @@ const router = new Router();
  *
  **/
 
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+  try {
+    let username = req.user.username;
+    let msg = await Message.get(req.params.id);
 
-/** POST / - post message.
+    if (msg.to_user.username !== username && msg.from_user.username !== username) {
+      throw new ExpressError("Cannot read this message", 401);
+    }
+
+    return res.json({message: msg});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
+
+/** post message.
  *
  * {to_username, body} =>
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
  **/
 
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+  try {
+    let msg = await Message.create({
+      from_username: req.user.username,
+      to_username: req.body.to_username,
+      body: req.body.body
+    });
 
-/** POST/:id/read - mark message as read:
+    return res.json({message: msg});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
+
+/** mark message as read:
  *
  *  => {message: {id, read_at}}
  *
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+
+router.post("/:id/read", ensureLoggedIn, async function (req, res, next) {
+  try {
+    let username = req.user.username;
+    let msg = await Message.get(req.params.id);
+
+    if (msg.to_user.username !== username) {
+      throw new ExpressError("Cannot set this message to read", 401);
+    }
+    let message = await Message.markRead(req.params.id);
+
+    return res.json({message});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
 
 module.exports = router;
