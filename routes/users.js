ba79942633a3a9703c0ec4c01e4adc62d9344faf
kelly <sntsklvn@gmail.com>
2023-06-21 10:20:21 -0400

    added functionality to get async methods on users model

---
+++ routes/users.js
@@ -0,0 +1,56 @@
+
+const db = require('../d');
+const express = require("express");
+const router = express.Router();
+const User =  require('../models/user');
+
+
+
+
+/** GET / - get list of users.
+ *
+ * => {users: [{username, first_name, last_name, phone}, ...]}
+ *
+ **/
+
+
+router.get('/', async (req, res, next)=>{
+
+
+    try{
+        const results = 
+        
+
+    }catch(e){
+
+        return next(e)
+    }
+})
+
+/** GET /:username - get detail of users.
+ *
+ * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
+ *
+ **/
+
+
+/** GET /:username/to - get messages to user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 from_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
+
+
+/** GET /:username/from - get messages from user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 to_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
