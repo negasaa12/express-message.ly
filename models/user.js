c76e2de986aea9986559f0598cc620b85a53e117
kelly <sntsklvn@gmail.com>
2023-06-21 10:00:09 -0400

    made changes to theUser model'

---
+++ models/user.js
@@ -0,0 +1,88 @@
+/** User class for message.ly */
+
+const { DB_URI } = require("../config");
+
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const ExpressError = require("../expressError");
+
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
+
+/** User of the site. */
+
+class User {
+
+  /** register new user -- returns
+   *    {username, password, first_name, last_name, phone}
+   */
+
+  static async register({username, password, first_name, last_name, phone}) { 
+        
+        let hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+        const results = await db.query(`
+        INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at) 
+        VALUES ($1, $2, $3,$4,$5, current_timestamp, current_timestamp) RETURNING username, password, first_name, last_name, phone`, [username,hashedPassword,first_name,last_name,phone]);
+
+        return results.rows[0];
+
+      }
+
+  /** Authenticate: is this username/password valid? Returns boolean. */
+
+  static async authenticate(username, password) { 
+
+      const results = await db.query(`SELECT password FROM users WHERE username = $1`, [username]);
+
+      let user = results.row[0]
+
+     return user && await bcrypt.compare(password,user.password);
+  }
+
+  /** Update last_login_at for user */
+
+  static async updateLoginTimestamp(username) { }
+
+  /** All: basic info on all users:
+   * [{username, first_name, last_name, phone}, ...] */
+
+  static async all() { }
+
+  /** Get: get user by username
+   *
+   * returns {username,
+   *          first_name,
+   *          last_name,
+   *          phone,
+   *          join_at,
+   *          last_login_at } */
+
+  static async get(username) { }
+
+  /** Return messages from this user.
+   *
+   * [{id, to_user, body, sent_at, read_at}]
+   *
+   * where to_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesFrom(username) { }
+
+  /** Return messages to this user.
+   *
+   * [{id, from_user, body, sent_at, read_at}]
+   *
+   * where from_user is
+   *   {username, first_name, last_name, phone}
+   */
+
+  static async messagesTo(username) { }
+}
+
+
+
+
+
+module.exports = User;
